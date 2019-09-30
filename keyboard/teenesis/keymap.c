2022e34efbf136a62bb5b695d50e2486d8f77c05
Oleg Kostyuk <cub.uanic@gmail.com>
2019-09-30 06:05:43 +0300

    bugnull: qq19

--- keyboard/teenesis/keymap.c
+++ keyboard/teenesis/keymap.c
@@ -471,6 +471,19 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
         clear_keyboard();
         print("\n\nJump to bootloader... ");
         _delay_ms(50);
+
+
+        cli();
+        UDCON = 1;
+        USBCON = (1<<FRZCLK);  // disable USB
+        UCSR1B = 0;
+        _delay_ms(5);
+        EIMSK = 0; PCICR = 0; SPCR = 0; ACSR = 0; EECR = 0; ADCSRA = 0;
+        TIMSK0 = 0; TIMSK1 = 0; TIMSK3 = 0; TIMSK4 = 0; UCSR1B = 0; TWCR = 0;
+        DDRB = 0; DDRC = 0; DDRD = 0; DDRE = 0; DDRF = 0; TWCR = 0;
+        PORTB = 0; PORTC = 0; PORTD = 0; PORTE = 0; PORTF = 0;
+        asm volatile("jmp 0x7E00");
+
         bootloader_jump(); // should not return
         print("not supported.\n");
     }
